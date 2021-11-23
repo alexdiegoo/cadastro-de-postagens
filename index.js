7ed723d517e31b02ff5dfb617fe6dc9f2b14b0e7
Alex Diego <alex.diegosn10@gmail.com>
2021-11-23 11:56:42 -0300

    Operações com db, cadastrar, apagar e exibir

--- index.js
+++ index.js
@@ -1,28 +1,54 @@
 const express = require('express');
 const app = express();
 const handlebars = require('express-handlebars');
+const bodyParser = require('body-parser');
+const Postagem = require('./models/Post');
+
 
 //config handlebars
 app.engine('handlebars', handlebars.engine({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 app.set('views', './views');
 
+// Body Parser
+app.use(bodyParser.urlencoded({extend: false}))
+app.use(bodyParser.json());
+
 // Conexão com db
 (async () => {
     const db = require('./models/db');
-    const Postagem = require('./models/Post');
-
     try {
-        const resultado = await db.sync();
-        console.log(resultado);
+        const connection = await db.sync();
     } catch (error) {
-        console.log(error);
+        console.log("Houve um erro" + error);
     }
 })();
 
-app.get("/", (req, res) => {
-  res.render('home');
+
+app.get("/", async (req, res) => {
+  const posts = await Postagem.findAll();
+  res.render('home', { posts });
 });
+
+app.get('/cadastrar', (req, res) => {
+  res.render('form');
+})
+
+app.get('/deletar/:id', async (req, res) => {
+  await Postagem.destroy({ where: { id: req.params.id}});
+  res.redirect('/');
+})
+
+app.post('/enviado', async (req, res) => {
+  const post = await Postagem.create({
+    titulo: req.body.titulo,
+    conteudo: req.body.conteudo
+  });
+
+  console.log(post)
+
+  res.redirect('/');
+})
 
 app.listen(8081, () => {
   console.log("Servidor iniciado em: http://localhost:8081/");
